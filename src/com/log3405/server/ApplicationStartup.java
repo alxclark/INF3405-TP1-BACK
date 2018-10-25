c175b1376e9148c6ddd49d28a2698d3468356a15
Grago <felix_agagnier@hotmail.ca>
2018-10-25 15:28:14 -0400

    Documentation

--- src/com/log3405/server/ApplicationStartup.java
+++ src/com/log3405/server/ApplicationStartup.java
@@ -6,26 +6,33 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.IO;
-
 public class ApplicationStartup {
 
+	/**
+	 * Setup the server on the specified IpAddress and port
+	 * @param args (not used)
+	 * @throws IOException
+	 */
 	public static void main(String[] args) throws IOException {
 		Scanner consoleInputReader = new Scanner(System.in);
 
+		//Read from console
 		String ipAddress = getIPAddress(consoleInputReader);
 		System.out.println("IPAddress is: " + ipAddress);
 		int port = getPort(consoleInputReader);
 		System.out.println("Port is: " + port);
 
+		//Setup server on specified IPaddress and port
 		ServerSocket listener = new ServerSocket();
 		InetAddress serverAddress = InetAddress.getByName(ipAddress);
 		listener.setReuseAddress(true);
 		listener.bind(new InetSocketAddress(serverAddress, port));
 
-		while (true) {//waits for new connections
+		while (true) {
 			try {
 				System.out.println("Waiting for a new connection ...");
+
+				//Starts a new thread for every connection request
 				Thread t = new Server(listener.accept());
 				t.start();
 			} catch (IOException e) {
@@ -35,25 +42,34 @@ public class ApplicationStartup {
 		}
 	}
 
+	/**
+	 * Read the IpAddress written into the console. Also compares it to a RegEx to validate the IpAddress Format
+	 * @param consoleInputReader Console Input Scanner
+	 * @return a String representing the IpAddress
+	 */
 	private static String getIPAddress(Scanner consoleInputReader) {
 		System.out.println("Hello, Please Enter the IPAddress of the server");
 		String ipAddressPattern = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
 		Pattern pattern = Pattern.compile(ipAddressPattern);
 		String ipAddress = consoleInputReader.nextLine();
 		Matcher matcher = pattern.matcher(ipAddress);
-		if (!matcher.matches())
-		{
+		if (!matcher.matches()) {
 			System.out.println("Invalid IPAddress. Valid example: 192.168.1.1");
 			ipAddress = getIPAddress(consoleInputReader);
 		}
 		return ipAddress;
 	}
 
+	/**
+	 * Read the port written into the console. Also verifies it's between 5000 and 5050
+	 * @param consoleInputReader Console Input Scanner
+	 * @return a Integer representing the port
+	 */
 	private static int getPort(Scanner consoleInputReader) {
 		System.out.println("Please Enter the Port of the server");
 		int port = consoleInputReader.nextInt();
-		if(port != 5000 && port != 5050){
-			System.out.println("Invalid port. Only 5000 and 5050 are supported");
+		if (port < 5000 && port > 5050) {
+			System.out.println("Invalid port. Only ports between 5000 and 5050 are supported");
 			port = getPort(consoleInputReader);
 		}
 		return port;
--- src/com/log3405/server/Server.java
+++ src/com/log3405/server/Server.java
@@ -15,6 +15,11 @@ public class Server extends Thread {
 	private final static String ROOT_DIRECTORY = "Server/Storage";
 	private final static int MAX_BUFFER_SIZE = 1024;
 
+	/**
+	 * Setup streams and inital directory, sends a message when ready
+	 * @param socket Socket assigned to this client
+	 * @throws IOException
+	 */
 	public Server(Socket socket) throws IOException {
 		this.socket = socket;
 		in = new DataInputStream(socket.getInputStream());
@@ -22,29 +27,38 @@ public class Server extends Thread {
 		System.out.println("[" + socket.getRemoteSocketAddress().toString() + " - " + LocalDateTime.now() + "]: Connected");
 		currentDirectory = new File(ROOT_DIRECTORY);
 
+		//send a connected confirmation to the client
 		byte[] starter = "Connected".getBytes();
 		BytesUtils.writeBytes(out, starter);
 	}
 
+	/**
+	 * Main loop of the thread
+	 */
 	public void run() {
 		try {
 			exec:
 			while (true) {
+				//read client command
 				byte[] received = BytesUtils.readBytes(in, MAX_BUFFER_SIZE, false);
 
-				// decryptPacket
+				//Transform packet into byte Array
+				//0 to 4: operation type code
+				//4 to end: data for the operation (named payload)
 				byte[] packetType = BytesUtils.extractSubByteArray(received, 0, 4);
 				byte[] packetPayload = BytesUtils.extractSubByteArray(received, 4, received.length);
-
 				int packetTypeAsInt = BytesUtils.bytesToInt(packetType);
+
+				//Log the IPAddress and time prefix
 				System.out.print("[" + socket.getRemoteSocketAddress().toString() + " - " + LocalDateTime.now() + "]: ");
 
 				switch (packetTypeAsInt) {
 					case 0: {// CD
-						System.out.println("cd " + BytesUtils.bytesToString(packetPayload));
+						//for the CD operation, the payload is a string with the target directory
 						String newDirCD = BytesUtils.bytesToString(packetPayload);
-						String messageCD;
+						System.out.println("cd " + newDirCD);
 
+						String messageCD;
 						if ("..".equals(newDirCD)) {
 							currentDirectory = currentDirectory.getParentFile();
 							messageCD = "Vous êtes dans le dossier " + currentDirectory.getName() + ".";
@@ -59,18 +73,22 @@ public class Server extends Thread {
 								messageCD = "Ce dossier n'existe pas.";
 							}
 						}
-						BytesUtils.writeBytes(out, messageCD.getBytes());
 
+						//send a message to the client with the result of the operation
+						BytesUtils.writeBytes(out, messageCD.getBytes());
 						break;
 					}
 					case 1: {// LS
+						//for the LS operation, the payload doesnt matter
 						System.out.println("ls");
 						listCurrentDirectory();
 						break;
 					}
 					case 2: { // mkdir
-						System.out.println("mkdir " + BytesUtils.bytesToString(packetPayload));
+						//for the mkdir operation, the payload is a string with the new directory name
 						String newDirMK = BytesUtils.bytesToString(packetPayload);
+						System.out.println("mkdir " + newDirMK);
+
 						String newPath = currentDirectory.getPath() + '/' + newDirMK;
 						String messageMK;
 						if (new File(newPath).mkdir()) {
@@ -78,24 +96,34 @@ public class Server extends Thread {
 						} else {
 							messageMK = "Il y a eu une erreur lors de la creation de ce dossier";
 						}
+
+						//send a message to the client with the result of the operation
 						BytesUtils.writeBytes(out, messageMK.getBytes());
 						break;
 					}
 					case 3: {// UPLOAD
+						//for the upload operation, the payload is a byte array that need to be divided further
+						//0 to 4: Number of bytes of the uploaded file
+						//4 to end: Name of the uploaded file
 						byte[] fileLengthPayload = BytesUtils.extractSubByteArray(packetPayload, 0, 4);
 						byte[] fileNamePayload = BytesUtils.extractSubByteArray(packetPayload, 4, packetPayload.length);
-						System.out.println("upload " + BytesUtils.bytesToString(fileNamePayload));
-
 						String newFileName = BytesUtils.bytesToString(fileNamePayload);
 						int fileLengthAsInt = BytesUtils.bytesToInt(fileLengthPayload);
+
+						System.out.println("upload " + newFileName);
+
+						//send a message to the client, to warn that the server is ready to receive the file
 						String messageMidUpload = "Ready to read:" + fileLengthAsInt + " bytes";
 						BytesUtils.writeBytes(out, messageMidUpload.getBytes());
 
+						//read file as bytes
 						byte[] file = BytesUtils.readBytes(in, fileLengthAsInt, true);
+
+						//Location of the new file
+						File newFile = new File(currentDirectory.getPath() + '/' + newFileName);
 						String messageUPLOAD;
 
-						File newFile = new File(currentDirectory.getPath() + '/' + newFileName);
-
+						//write the bytes to the file
 						try (FileOutputStream fos = new FileOutputStream(newFile.getPath())) {
 							if (!newFile.exists()) {
 								newFile.createNewFile();
@@ -106,40 +134,58 @@ public class Server extends Thread {
 							messageUPLOAD = "Une erreur est arrivee lors du televersement du fichier";
 						}
 
+						//send a message to the client with the result of the operation
 						BytesUtils.writeBytes(out, messageUPLOAD.getBytes());
 						break;
 					}
 					case 4: {// DOWNLOAD
+						//for the download operation, the payload is a string with the target file
 						String targetFileName = BytesUtils.bytesToString(packetPayload);
 						System.out.println("download " + targetFileName);
 						File targetFile = new File(currentDirectory.getPath() + '/' + targetFileName);
+
+						//if the file exists, continue the operation, else send an error to the client
 						if (targetFile.exists()) {
+							//Send a packet with the file structure
+							//0 to 4: Status code for the file (0 = normal), (1 = error)
+							//4 to 8: Length in bytes of the file
+							//8 to end: Length of the file name
 							byte[] fileStatus = BytesUtils.intToBytes(0);
 							byte[] filePayload = Files.readAllBytes(targetFile.toPath());
-							byte[] dFileLengthPayload = BytesUtils.intToBytes(filePayload.length);
-							byte[] dFileNamePayload = targetFile.getName().getBytes();
-							byte[] tempPacket = BytesUtils.concat(fileStatus, dFileLengthPayload);
-							byte[] finalPacket = BytesUtils.concat(tempPacket, dFileNamePayload);
+							byte[] fileLengthPayload = BytesUtils.intToBytes(filePayload.length);
+							byte[] fileNamePayload = targetFile.getName().getBytes();
+							byte[] tempPacket = BytesUtils.concat(fileStatus, fileLengthPayload);
+							byte[] finalPacket = BytesUtils.concat(tempPacket, fileNamePayload);
 
 							BytesUtils.writeBytes(out, finalPacket);
+
+							//read until client is ready to handle the file
 							System.out.println("[" + socket.getRemoteSocketAddress().toString() + " - " + LocalDateTime.now() + "]:");
-							System.out.println(
-									BytesUtils.bytesToString(BytesUtils.readBytes(in, MAX_BUFFER_SIZE, false)));//read until client is ready to handle the file
+							System.out.println(BytesUtils.bytesToString(BytesUtils.readBytes(in, MAX_BUFFER_SIZE, false)));
+
+							//send a packet containing only the file's bytes
 							BytesUtils.writeBytes(out, filePayload);
 						} else {
+							//Send a packet describing the error
+							//0 to 4: Status code for the file (0 = normal), (1 = error)
+							//4 to end: error message
 							byte[] fileStatus = BytesUtils.intToBytes(1);
 							byte[] responsePayload = ("File " + targetFile.getName() + " does not exists").getBytes();
 							byte[] finalPacket = BytesUtils.concat(fileStatus, responsePayload);
 
+							//send packet
 							BytesUtils.writeBytes(out, finalPacket);
 						}
 
 						break;
 					}
 					case 5: {// EXIT
+						//for the exit operation, the payload doesnt matter
 						System.out.println("exit");
+						//send a message to the client with the result of the operation
 						BytesUtils.writeBytes(out, "Vous avez été déconnecté avec succès.".getBytes());
-						// disconnect now ?
+
+						// break the loop to excute the closing functions
 						break exec;
 					}
 				}
@@ -158,28 +204,37 @@ public class Server extends Thread {
 		}
 	}
 
+	/**
+	 * Builds a String for the directory structure and send it to the client
+	 * @throws IOException thrown in case of an error with the sockets
+	 */
 	private void listCurrentDirectory() throws IOException {
 		File[] directoryListing = currentDirectory.listFiles();
+		StringBuilder builder = new StringBuilder();
 		if (directoryListing != null) {
-			String response = "";
 			for (File child : directoryListing) {
 				if (child.isDirectory()) {
-					response += "[ FOLDER ] ";
+					builder.append("[ FOLDER ] ");
 				} else {
-					response += "[ FILE ] ";
+					builder.append("[ FILE ] ");
 				}
-				response += child.getName() + "\n";
+				builder.append(child.getName()).append("\n");
 			}
-			if ("".equals(response)) {
+			if ("".equals(builder.toString())) {
 				BytesUtils.writeBytes(out, "[ EMPTY ]".getBytes());
 			} else {
-				BytesUtils.writeBytes(out, response.getBytes());
+				BytesUtils.writeBytes(out, builder.toString().getBytes());
 			}
 		} else {
 			BytesUtils.writeBytes(out, "There was an error with this directory".getBytes());
 		}
 	}
 
+	/**
+	 * Close the streams and sockets
+	 * @param in InputStream of the socket
+	 * @param out OutputStream of the socket
+	 */
 	private void close(InputStream in, OutputStream out) {
 		try {
 			System.out.println("Closing socket and I/O streams");
